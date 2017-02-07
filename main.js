532b918df0ff1dfe99720bdf3878ed82b3b97d89
Nicolás <diz.nicolasamor@gmail.com>
2017-02-07 01:14:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,28 +11,58 @@ API from http://openweathermap.org/api
 $(document).ready(function() {
   
   var loc;
-//My API key:   f0c304536bcea0bc10f6fdb0a3dd88b2  
+  //My API key:   f0c304536bcea0bc10f6fdb0a3dd88b2  
   var myAppid = "f0c304536bcea0bc10f6fdb0a3dd88b2";
 
+  //convert cel to fahrenheit. 
+  function celToFahr(num){
+    var fahr = parseInt(num * (9 / 5) + 32);
+    return fahr;
+  }
+
+  //convert cel to fahrenheit. 
+  function fahrToCel(num2){
+    var cel = parseInt( (num2-32) * 5 / 9);
+    return cel; 
+  }
+
+  
   $.getJSON('http://ipinfo.io', function(data){
     //loc is an array with lat = loc[0] && lon = loc[1]
     loc = data.loc.split(",");
-    
+
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+loc[0]+"&lon="+loc[1]+"&appid="+myAppid, function(weatherData){
-     
-      $("#description").html(weatherData.weather[0].description),
-      $("#city").html(weatherData.name+", "+weatherData.sys.country),
-      $("#temperature").html(weatherData.main.temp), 
-      $("#maxTemp").html(weatherData.main.temp_max),        
-      $("#minTemp").html(weatherData.main.temp_min),
-      $("#wind").html(weatherData.wind.speed);
+console.log(weatherData);     
+      var iconVar = weatherData.weather[0].icon;
+      
+      //the API gives me the temp in kelvin. Remember that cel = kelvin - 273;
+      var tempCel = parseInt(weatherData.main.temp - 273);
+      var tempFahr = parseInt( (tempCel-32) * 5 / 9 );
+      var tempMinCel = parseInt(weatherData.main.temp_min - 273);
+      var tempMaxCel = parseInt(weatherData.main.temp_max - 273);
+
+      $("#city").html(weatherData.name+", "+weatherData.sys.country);
+      $("#icon").html("<img src='http://openweathermap.org/img/w/"+iconVar+".png' height='100'>");
+      $("#description").html(weatherData.weather[0].description);
+      $("#temperature").html(tempCel + "ºC"); 
+      $("#minTemp").html("Min.: " + tempMinCel + " ºC"); 
+      $("#maxTemp").html("Max.: " + tempMaxCel + " ºC"); 
+      
+      //convert cel to fahr && fahr to cel
+      $("#convert").click(function () {
+        if ($('#temperature').text().indexOf('F') > -1) {
+          $('#temperature').text(tempCel + '° C');
+          $("#minTemp").text("Min.: " + tempMinCel + '° C');
+          $("#maxTemp").text("Max.: " + tempMaxCel + '° C');
+        } else {
+          $('#temperature').text(celToFahr(tempCel) + '° F');
+          $("#minTemp").text("Min.: " + celToFahr(tempMinCel) + '° F');
+          $("#maxTemp").text("Max.: " + celToFahr(tempMaxCel) + '° F');
+        }
+      });
 
     })  // get JSON weatherData
     
   })  //get JSON ipinfo
   
 })  //document ready
-  
-  //To do.  Change background according to the data. 
-//  var body = getElementById('body'); 
-//  body.style.backgroundColor =  //.......
