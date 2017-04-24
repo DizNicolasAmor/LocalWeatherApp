acb01e1b05b309eb2743c6023707d5d0d09f6b75
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2017-04-24 19:33:45 -0300

    Put all the API call inside function success()

--- main.js
+++ main.js
@@ -25,15 +25,54 @@ $(document).ready(function() {
     var cel = parseInt( (num2-32) * 5 / 9);
     return cel; 
   }
-
-  function getLocation(callback){
+    
+  function getLocation(){
     console.log('Searching location info...');
     
     function success(position) {
       var lat  = position.coords.latitude;
       var lon = position.coords.longitude;
       console.log('lat: '+lat+' ; '+'lon: '+lon);
-    }
+
+      console.log('Searching weather info from...');
+      
+      var urlString = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+myAppid;
+      console.log(urlString);
+    
+      $.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+myAppid, function(weatherData){
+        
+        console.log(weatherData);
+        var iconVar = weatherData.weather[0].icon;
+        
+        //the API gives me the temp in kelvin. Remember that cel = kelvin - 273;
+        var tempCel = parseInt(weatherData.main.temp - 273);
+        var tempFahr = parseInt( (tempCel-32) * 5 / 9 );
+        var tempMinCel = parseInt(weatherData.main.temp_min - 273);
+        var tempMaxCel = parseInt(weatherData.main.temp_max - 273);
+        
+        $("#city").html(weatherData.name+", "+weatherData.sys.country);
+        $("#icon").html("<img src='http://openweathermap.org/img/w/"+iconVar+".png' height='100'>");
+        $("#description").html(weatherData.weather[0].description);
+        $("#temperature").html(tempCel + "ºC"); 
+        $("#minTemp").html("Min.: " + tempMinCel + " ºC"); 
+        $("#maxTemp").html("Max.: " + tempMaxCel + " ºC"); 
+        
+        //convert cel to fahr && fahr to cel
+        $("#convert").click(function () {
+          if ($('#temperature').text().indexOf('F') > -1) {
+            $('#temperature').text(tempCel + '° C');
+            $("#minTemp").text("Min.: " + tempMinCel + '° C');
+            $("#maxTemp").text("Max.: " + tempMaxCel + '° C');
+          } else {
+            $('#temperature').text(celToFahr(tempCel) + '° F');
+            $("#minTemp").text("Min.: " + celToFahr(tempMinCel) + '° F');
+            $("#maxTemp").text("Max.: " + celToFahr(tempMaxCel) + '° F');
+          }
+        });
+        
+      })  // get JSON weatherData
+      
+    } //success
     
     function error() {
       $('#city').html("Unable to retrieve your location");
@@ -44,48 +83,9 @@ $(document).ready(function() {
     }
     
     navigator.geolocation.getCurrentPosition(success, error);
-
-    callback();
-
-  };//getLocation(); 
-
-  function getApiData(){
-      $.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+myAppid, function(weatherData){
-        
-        var urlString = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+myAppid;
-        console.log(urlString);
-        console.log(weatherData);     
-        var iconVar = weatherData.weather[0].icon;
     
-    //the API gives me the temp in kelvin. Remember that cel = kelvin - 273;
-      var tempCel = parseInt(weatherData.main.temp - 273);
-      var tempFahr = parseInt( (tempCel-32) * 5 / 9 );
-      var tempMinCel = parseInt(weatherData.main.temp_min - 273);
-      var tempMaxCel = parseInt(weatherData.main.temp_max - 273);
-
-      $("#city").html(data.city+", "+data.country);
-      $("#icon").html("<img src='http://openweathermap.org/img/w/"+iconVar+".png' height='100'>");
-      $("#description").html(weatherData.weather[0].description);
-      $("#temperature").html(tempCel + "ºC"); 
-      $("#minTemp").html("Min.: " + tempMinCel + " ºC"); 
-      $("#maxTemp").html("Max.: " + tempMaxCel + " ºC"); 
-      
-      //convert cel to fahr && fahr to cel
-      $("#convert").click(function () {
-        if ($('#temperature').text().indexOf('F') > -1) {
-          $('#temperature').text(tempCel + '° C');
-          $("#minTemp").text("Min.: " + tempMinCel + '° C');
-          $("#maxTemp").text("Max.: " + tempMaxCel + '° C');
-        } else {
-          $('#temperature').text(celToFahr(tempCel) + '° F');
-          $("#minTemp").text("Min.: " + celToFahr(tempMinCel) + '° F');
-          $("#maxTemp").text("Max.: " + celToFahr(tempMaxCel) + '° F');
-        }
-      });
-
-    })  // get JSON weatherData
-  };
+  };//getLocation(); 
   
-  getLocation(getApiData);
-
+  getLocation();
+  
 });//document ready
